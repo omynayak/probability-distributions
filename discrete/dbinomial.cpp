88e7bb556e03145dcd660daa09935180aca9d6ca
omynayak <manelomnayak099@gmail.com>
2025-11-14 12:08:50 +0530

    Minor improvements to Discrete Binomial

--- discrete/dbinomial.cpp
+++ discrete/dbinomial.cpp
@@ -3,18 +3,32 @@
 std::vector<double> binomial_pmf(size_t n, double p)
 {
     std::vector<double> probabilities;
-    probabilities.reserve(n);
+    probabilities.reserve(n+1);
+    if (p == 0.0) {
+        probabilities.assign(n + 1, 0.0);
+        probabilities[0] = 1.0;
+        return probabilities;
+    }
+
+    if (p == 1.0) {
+        probabilities.assign(n + 1, 0.0);
+        probabilities[n] = 1.0;
+        return probabilities;
+    }
+
     double q = 1 - p;
-    
+    double logq = std::log(q);
+    double logp = std::log(p);
+    double log_n_fact = std::lgamma(n+1);
     for(size_t r{0}; r <= n; r++){
         double log_comb = 
-        std::lgamma(n + 1) -
+        log_n_fact -
         ( std::lgamma(n - r + 1) +
         std::lgamma(r + 1));
 
         double log_prob = 
-        r * std::log(p) +
-        (n - r) * std::log(q);
+        r * logp +
+        (n - r) * logq;
 
         double probability = std::exp(log_comb + log_prob);
         probabilities.push_back(probability);
@@ -25,14 +39,19 @@ std::vector<double> binomial_pmf(size_t n, double p)
 
 double binomial_pmf(size_t n, size_t r, double p)
 {
+    double q = 1 - p;
+    double logq = std::log(q);
+    double logp = std::log(p);
+    double log_n_fact = std::lgamma(n + 1);
+
     double log_comb = 
-    std::lgamma(n + 1) -
+    log_n_fact -
     ( std::lgamma(n - r + 1) +
     std::lgamma(r + 1));   
 
     double log_prob = 
-        r * std::log(p) +
-        (n - r) * std::log(1-p);
+        r * logp +
+        (n - r) * logq;
 
     return std::exp(log_comb + log_prob);
 }
