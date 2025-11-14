4537bf9a1555efdc624c81021a36fc06649e7346
omynayak <manelomnayak099@gmail.com>
2025-11-14 12:16:32 +0530

    Complete Discrete Uniform Distribution

--- discrete/duniform.cpp
+++ discrete/duniform.cpp
@@ -1,5 +1,16 @@
 #include "ddist.h"
 
-std::vector<double> uniform_pmf(size_t n){}
+std::vector<double> uniform_pmf(size_t n)
+{
+    std::vector<double> probabilities(n);
+    double val = 1.0 / (n);
 
-double uniform_pmf(size_t n, size_t k){}
+    std::fill(probabilities.begin(), probabilities.end(), val);
+    return probabilities;
+}
+
+double uniform_pmf(size_t n, size_t k)
+{
+    if (k == 0 || k > n) return 0.0;
+    return 1.0 / n;
+}
